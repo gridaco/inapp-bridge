18f50ac39fb61e12a8b6c1f608dbddd8e3e9a60b
woojoo <woojoo@softmarshmallow.com>
2020-01-23 17:26:10 +0900

    👉 updated `web/readme`

--- web/examples/demo/src/App.tsx
+++ web/examples/demo/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import { Test1 } from '../../..';
 const App: React.FC = () => {
   return (
     <div className="App">
